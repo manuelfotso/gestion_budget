25cbc3b4fce044405905b8d66d9283b071eb3ac1
NGOUEMO Iness <ngouemoiness@gmail.com>
2025-03-10 12:53:46 +0100

    mon premier depot

---
+++ gestion_budget/BACKEND/gestion_budget/src/test/java/com/iuc/gestion_budget/GestionBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuc.gestion_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
